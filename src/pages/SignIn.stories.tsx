32bbd08c61f71fb5f7360b6be05f9419faaad8ec
Milena Boselli Rosa <milena.rosa@gmail.com>
2022-10-14 21:49:08 -0300

    created sign in page, tests and msw

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,43 @@
+import { SignIn } from "./SignIn";
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { rest } from "msw";
+
+export default {
+  title: "Pages/Sign In",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText("Digite seu e-mail"),
+      "milena.rosa@gmail.com"
+    );
+    userEvent.type(canvas.getByPlaceholderText("********"), "12345678");
+
+    userEvent.click(canvas.getByRole("button"));
+
+    await waitFor(() =>
+      expect(canvas.getByText("Login realizado!")).toBeInTheDocument()
+    );
+  },
+};
